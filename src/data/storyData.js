0be3b75ccb3badc7464b94580d838a04033e39a8
Siva-jerry <msiva0127@gmail.com>
2026-08-17 13:32:09 +0530

    URL update

--- src/data/storyData.js
+++ src/data/storyData.js
@@ -879,7 +879,7 @@ const storyData = [
 
     trailer: {
       youtubeId: "I_kDb-pRCds",
-      url: "https://youtu.be/17ywQS6XO-M?si=sn_3_eCfFZsK8id1",
+      url: "https://youtu.be/euqRKhYnqFQ?si=CDOQCSH6UgKWVOWv",
       channel: "Warner Bros. Pictures",
       label: {
         en: "Official Trailer",
